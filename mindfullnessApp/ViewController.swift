29e5c09bdac055721dfaf24d64a03ef9e956b200
kobayashi-kenta <12321klj@gmail.com>
2020-12-28 15:38:50 +0900

    UIデザインの実装

---
+++ mindfullnessApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  mindfullnessApp
+//
+//  Created by Kenta on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
